93d90809cb477c1d2ff550d1af6bc1341d0f19d0
Kevinmt24 <kevinmt01@gmail.com>
2021-05-16 11:29:10 +0530

    Function definition added

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,16 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+     x <<- y
+     inv <<- NULL
+  }
+  get <- function() {x}  ##Getting the matrix itself
+  setInverse <- function(inverse) {inv <<- inverse} ##Change the value to inverse
+  getInverse <- function() {inv}  ##get the inverse value
+  list(set = set, get = get,setInverse = setInverse,getInverse = getInverse)
+  
 }
 
 
@@ -12,4 +21,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+  inv <- x$getInverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
+  }
